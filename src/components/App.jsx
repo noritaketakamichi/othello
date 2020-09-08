58758102426424fc4bef4e6c82a96794216433be
noritaketakamichi <noritaket28@gmail.com>
2020-09-08 22:12:16 +0900

    useeffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,34 +4,42 @@ import Stone from './Stone';
 // import '../styles/styles.scss';
 
 export default function App() {
-	// const initialStones = [];
-	// for (let i = 0; i < 8; i++) {
-	// 	initialStones.push([]);
-	// 	for (let j = 0; j < 8; j++) {
-	// 		initialStones[i].push([]);
-	// 	}
-	// }
 
-	const arr = [1, 2, 3];
 	const array = [];
 	for (let row = 0; row < 8; row += 1) {
 		array.push([]);
 		for (let col = 0; col < 8; col += 1) {
-			array[row].push([row,col,"-"]);
+			array[row].push([row, col, '-']);
 		}
 	}
+	
+	const setInitialCondition = function () {
+		const array = [];
+		for (let row = 0; row < 8; row += 1) {
+			array.push([]);
+			for (let col = 0; col < 8; col += 1) {
+				array[row].push([row, col, '-']);
+			}
+		}
 
-	array[3][3][2] = 'w';
-	array[3][4][2] = 'b';
-	array[4][3][2] = 'b';
-	array[4][4][2] = 'w';
+		array[3][3][2] = 'w';
+		array[3][4][2] = 'b';
+		array[4][3][2] = 'b';
+		array[4][4][2] = 'w';
+		return array;
+	};
 
-	// const [stones, setStones] = useState(initialStones);
+	
+	useEffect(() => {
+		setCondition(setInitialCondition())
+	}, []);
+
+	const [condition, setCondition] = useState(array);
 
 	return (
 		<>
-			<Board array={array}/>
-			<Stone array={array} />
+			<Board array={condition} />
+			<Stone array={condition} />
 		</>
 	);
 }
