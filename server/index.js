4fa76827372638fb76a69fa7b560e2709e30611b
noritaketakamichi <noritaket28@gmail.com>
2020-09-14 11:29:26 +0900

    add packages

--- server/index.js
+++ server/index.js
@@ -1,16 +1,11 @@
-require("dotenv").config();
+const express = require("express");
+const path = require("path");
 
-const PORT = process.env.PORT || 9000;
+const app = express();
+app.use(express.static(path.resolve(__dirname, "dist")));
 
-(async() => {
-    try {
-        console.log("Running migrations");
-        await db.migrate.latest();
-
-        console.log("Starting express");
-        app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
-    } catch (err) {
-        console.error("Error starting app!", err);
-        process.exit(-1);
-    }
-})();
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    // eslint-disable-next-line
+    console.log(`Server up and listening on port ${port}`);
+});
